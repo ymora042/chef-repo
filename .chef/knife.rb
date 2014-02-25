b504e7bceb45c0ae9dd4b99f11e0611e078be2e1
unknown <SaraiM@SARAIMW7X6540.us.corp>
2014-02-24 21:15:48 -0500

    The started kit from chef

---
+++ .chef/knife.rb
@@ -0,0 +1,13 @@
+# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "ymora042"
+client_key               "#{current_dir}/ymora042.pem"
+validation_client_name   "ultimate_software-validator"
+validation_key           "#{current_dir}/ultimate_software-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/ultimate_software"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
